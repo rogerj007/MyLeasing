593694d5ac31989d8bcfd0afafb53dbc5a520f00
rogerj007 <roger_j007@outlook.com>
2019-08-16 22:27:19 -0500

    Carga Inicial

---
+++ MyLeasing.Web/Data/Entities/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<Lessee> Lessees { get; set; }
+        public DbSet<Property> Properties { get; set; }
+        public DbSet<PropertyImage> PropertyImages { get; set; }
+        public DbSet<PropertyType> PropertyTypes { get; set; }
+    }
+
+}
